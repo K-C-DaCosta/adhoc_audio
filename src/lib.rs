50cdb2b23c5ba4533514b823bc25801caefae41c
khadeem dacosta <khadeem.dacosta@gmail.com>
2021-11-29 20:21:18 -0500

    made SeekFrom reachable from my crate for ease of use purposes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod codec;
 pub mod collections;
 pub mod math;
-pub use codec::{adhoc::AdhocCodec, wav::WavCodec,StreamInfo};
+
+
+pub use codec::{adhoc::AdhocCodec, wav::WavCodec, StreamInfo};
+pub use std::io::SeekFrom;
