d67c96a3208ec443551bea2055bfc8a85d86d734
khadeem dacosta <khadeem.dacosta@gmail.com>
2021-12-02 22:39:38 -0500

    adding new version

--- src/codec.rs
+++ src/codec.rs
@@ -2,13 +2,15 @@ use super::{
     collections::{BitStream, CircularStack, NibbleList},
     math::FixedParabola,
 };
+use serde::{Deserialize, Serialize};
 pub use std::io::{Seek, SeekFrom};
 
+/// The first codec in this crate that actually compresses things
 pub mod adhoc;
-pub use adhoc::*;
-use serde::{Deserialize, Serialize};
-
+/// A utility for Reading/Writing wav files
 pub mod wav;
+
+pub use adhoc::*;
 
 const NORMALIZE_FACTOR: f32 = 1.0 / i16::MAX as f32;
 
@@ -25,6 +27,9 @@ fn truncate_sample(samp: f32) -> i16 {
 }
 
 #[derive(Copy, Clone, Default, Serialize, Deserialize)]
+/// A POD that contains basic information about an audio signal
+/// ## Comments
+/// If you instantiate a codec make sure this is set before you encode(..)/decode(..)
 pub struct StreamInfo {
     /// `sample_rate` is in Hz so typically you set this to something like: `44_100` or `48_000`
     sample_rate: u32,
@@ -52,22 +57,21 @@ impl StreamInfo {
         unsafe { std::slice::from_raw_parts(data, std::mem::size_of::<StreamInfo>()) }
     }
 }
-
+/// all codecs in this crate implement this trait to expose the `encode(..)`,`decode(..)` and `seek(..)` routines
 /// # Description
 /// Designed for use in the browser so all audio data is assumed to be PCM INTERLEAVED with IEE754 values ranging from -1.0 to 1.0
 /// ## Comments
 /// - Some people may think it unusual to do audio stuff in f32 but WEBAUDIO API pretty much forces me to use them
 pub trait Streamable {
-    
-    /// # Description 
+    /// # Description
     /// returns fundamental information about the stream
     fn info(&self) -> StreamInfo;
 
-    /// # Description 
-    /// returns a **tight upperbound** of bits neeeded to store encoded data 
+    /// # Description
+    /// returns a **tight upperbound** of bits neeeded to store encoded data
     /// ## Comments
     /// - can be used to set filesize limits
-    fn filesize_upperbound(&self)->u64;
+    fn filesize_upperbound(&self) -> u64;
 
     /// # Description
     /// encodes `samples` and returns number of samples encoded
@@ -81,15 +85,18 @@ pub trait Streamable {
 
     /// # Description
     /// Seeks to a certain spot in the stream
-    /// ## Parameters 
-    /// - `dt` is change in time in milliseconds 
+    /// ## Parameters
+    /// - `dt` is change in time in milliseconds
     /// ## Comments
     /// - Notes about `AdhocCodec`:
-    ///     - Intented to be used **ONLY** after you've completely finished encoding \
+    ///     - Currently only `SeekFrom::Start` is implemented 
+    ///     - Intented to be used **ONLY AFTER** you've completely finished encoding \
     ///     audio, or you have just loaded the codec for the first time
     ///     - with `AdhocCodec` you can't just seek to a random spot and start encoding
-    ///     - If you want to reuse the memory allocated call `AdhocCodec::init()` \
-    ///     before encoding to reuse the stream memory allocated
-    ///     - Currently only `SeekFrom::Start` is implemented
+    ///     - If you want to reuse the memory allocated call `AdhocCodec::init()` or `dt=SeekFrom::Start(0)` \
+    ///     before encoding. This will reuse the stream memory allocated.
+    /// - Notes about `WavCodec`:
+    ///     - `SeekFrom::Start`, `SeekFrom::Current` and `SeekFrom::End` is implemented
+    ///     - there no restrictions on how one should call this after encode/decode
     fn seek(&mut self, dt: SeekFrom);
 }
